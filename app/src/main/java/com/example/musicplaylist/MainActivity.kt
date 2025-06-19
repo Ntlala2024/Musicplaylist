189645723a46713c457e29d98114c2a6f265d758
Ntlala2024 <ST10479927@rcconnect.edu.za>
2025-06-19 08:43:18 +0000

    Initial commit

---
+++ app/src/main/java/com/example/musicplaylist/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.musicplaylist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.musicplaylist.ui.theme.MusicPlaylistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            var number1 by remember { mutableStateOf("") }
+
+            var number2 by remember { mutableStateOf("") }
+
+            var result by remember { mutableStateOf("") }
+
+            Row {
+                Button(onClick = {}) { }
+            }
+        }
+    }
+}
